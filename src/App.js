164de84a6111fb0603fae0de451cc539080d2424
John Otander <johnotander@gmail.com>
2016-09-16 14:14:53 -0600

    Add basic spacing support

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ class App extends Component {
     const config = {
       typeScale: [
         10, 8, 1
-      ]
+      ],
+      spacing: {
+        root: 4,
+        ratio: 1.5,
+        steps: 6
+      }
     }
 
     this.state = {
@@ -19,10 +24,21 @@ class App extends Component {
     }
   }
 
+  handleSpacing (key, val) {
+    const newConfig = Object.assign({}, this.state.config)
+    newConfig.spacing[key] = val
+
+    this.setState({
+      config: newConfig,
+      spacing: generator(newConfig).spacing()
+    })
+  }
+
   handleTypeScale (i, val) {
-    const newConfig = this.state.config
+    const newConfig = Object.assign({}, this.state.config)
     const newTypeScale = newConfig.typeScale
-    newTypeScale[i] = parseInt(val)
+
+    newTypeScale[i] = val
     newConfig.typeScale = newTypeScale
 
     this.setState({
@@ -32,13 +48,15 @@ class App extends Component {
   }
 
   render () {
-    const { typeScale, config } = this.state
+    const { typeScale, config, spacing } = this.state
 
     return (
       <div>
         <style>
           {typeScale}
+          {spacing}
         </style>
+
         <span className='f1'>f1</span>
         <span className='f2'>f2</span>
         <span className='f3'>f3</span><br />
@@ -46,6 +64,11 @@ class App extends Component {
         <input value={config.typeScale[1]} onChange={e => this.handleTypeScale(1, e.target.value)} />
         <input value={config.typeScale[2]} onChange={e => this.handleTypeScale(2, e.target.value)} />
         <pre><code>{typeScale}</code></pre>
+
+        <input value={config.spacing.steps} onChange={e => this.handleSpacing('steps', e.target.value)} />
+        <input value={config.spacing.ratio} onChange={e => this.handleSpacing('ratio', e.target.value)} />
+        <input value={config.spacing.root} onChange={e => this.handleSpacing('root', e.target.value)} />
+        <pre><code>{spacing}</code></pre>
       </div>
     )
   }
